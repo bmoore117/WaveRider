d4956d34fe2532494d9a150c7cdca70514063406
Benjamin Moore <american.exceptionalist@gmail.com>
2017-04-27 16:32:52 -0400

    Improved maxima and minima

--- src/main/scala/com/leetcode/waverider/utils/MathUtils.scala
+++ src/main/scala/com/leetcode/waverider/utils/MathUtils.scala
@@ -7,9 +7,13 @@ import scala.collection.mutable
   */
 object MathUtils {
 
-  def findLocalMaxima(list:List[Double]):List[Double] = {
+  def findLocalMaxima(list:List[Double]):List[Int] = {
 
-    var results = new mutable.ArrayBuffer[Double]()
+    if(list.length < 2) {
+      return List[Int]()
+    }
+
+    var results = new mutable.ArrayBuffer[Int]()
     var prevPt = list.head
 
     var isUp = false
@@ -19,8 +23,8 @@ object MathUtils {
       val point = list(i)
 
       if(prevPt < point) {
-        if(i == list.length - 1 && isUp) {
-          results += i - 1
+        if(i == list.length - 1) {
+          results += i
         }
         isUp = true
       } else if(prevPt > point) {
@@ -30,6 +34,46 @@ object MathUtils {
         isUp = false
       }
       prevPt = point
+    }
+
+    if(results.isEmpty && list.head == list.max) {
+      results += 0
+    }
+
+    results.toList
+  }
+
+  def findLocalMinima(list:List[Double]):List[Int] = {
+
+    if(list.length < 2) {
+      return List[Int]()
+    }
+
+    var results = new mutable.ArrayBuffer[Int]()
+    var prevPt = list.head
+
+    var isDown = false
+
+    for(i <- list.indices) {
+
+      val point = list(i)
+
+      if(prevPt > point) {
+        if(i == list.length - 1) {
+          results += i
+        }
+        isDown = true
+      } else if(prevPt < point) {
+        if(isDown) {
+          results += i - 1
+        }
+        isDown = false
+      }
+      prevPt = point
+    }
+
+    if(results.isEmpty && list.head == list.min) {
+      results += 0
     }
 
     results.toList
